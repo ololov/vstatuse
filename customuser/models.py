9a87aa480f153e5c126485e868835774e4adb900
Yegor Kowalew <kowalew.backup@gmail.com>
2010-09-12 16:32:48 +0300

    ????????? ?????? ?????????, ????????????, ??????????? ?????? ?????????? ?????

--- customuser/models.py
+++ customuser/models.py
@@ -11,9 +11,12 @@ class CustomUser(User):
     identity = models.CharField(max_length=50, verbose_name='URL идентификатор пользователя', blank=True, null=True)
     provider = models.CharField(max_length=50, verbose_name='URL провайдера', blank=True, null=True)
     uid = models.CharField(max_length=50, verbose_name='uid', blank=True, null=True)
-    #nickname = models.CharField(max_length=50, verbose_name='nickname', blank=True, null=True)
     gender = models.CharField(max_length=50, verbose_name='gender', blank=True, null=True)
     photo = models.CharField(max_length=50, verbose_name='photo', blank=True, null=True)
+    max_votes_yes = models.IntegerField(verbose_name='Всего голосов за', blank=True, null=True)
+    max_votes_no = models.IntegerField(verbose_name='Всего голосов против', blank=True, null=True)
+    user_rating = models.FloatField(verbose_name='Рейтинг пользователя', blank=True, null=True)
+    status_count = models.IntegerField(verbose_name='Всего статусов', blank=True, null=True)
     objects = UserManager()
     class Meta:
         verbose_name = "пользователя"
@@ -22,12 +25,12 @@ class CustomUser(User):
 '''
 Надо будет добавить.
 rr = {
-'max_rating': 0,
-'name': u'Katerina',
-'votes_yes': 0,
-'votes_no': 0,
-'autor_rating': 0.0,
-'status_count': 1,
+'max_rating': 0, выбрасываю, зачем на столько нагружать
+'name': u'Katerina', есть
+'votes_yes': 0, есть
+'votes_no': 0, есть
+'autor_rating': 0.0, есть
+'status_count': 1, есть
 'id': 111,
 'date_joined': datetime.datetime(2010, 9, 12, 4, 57, 1, 642738)}
 '''
--- status/models.py
+++ status/models.py
@@ -5,11 +5,45 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+import pytils
 
 STATUS_STATUS = (
     ('d', 'Не утверждена'),
     ('p', 'Утверждена'),
 )
+
+class RandomText(models.Model):
+    '''Подписи в шапке сайта'''
+    random_text_body = models.TextField(max_length=50, verbose_name='Текст')
+    random_text_status = models.CharField(max_length=1, choices=STATUS_STATUS, verbose_name=u'Статус блока', default='p')
+
+    def __unicode__(self):
+        return self.random_text_body
+
+    class Meta:
+        verbose_name = "случайный блок"
+        verbose_name_plural = "Случайные блоки"
+
+class Category(models.Model):
+    '''Модель категорий (тегов)'''
+    category_name = models.CharField(max_length=50, verbose_name=u'Название')
+    category_slug = models.CharField(max_length=50, verbose_name=u'Служебное поле', unique=True, blank=True)
+    category_status_count = models.IntegerField(verbose_name=u'Количество статусов в категории', blank=True, null=True, default=0)
+
+    def save(self):
+        if not self.category_slug:
+            self.category_slug = pytils.translit.slugify(self.category_name)
+        super(Category, self).save()
+
+    def __unicode__(self):
+        return self.category_name
+
+    def get_absolute_url(self):
+        return "/category/%s/" % self.category_slug
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
 
 class VStatus(models.Model):
     '''Модель статусов'''
